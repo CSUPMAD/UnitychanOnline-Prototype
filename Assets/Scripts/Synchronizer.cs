b6b8b969888c6f28837a944e478f465011a8199f
CSUPMAD <ex.large@gmail.com>
2014-07-28 03:51:44 +0900

    add Lerp
    add lerp of motion of others connected.

--- Assets/Scripts/Synchronizer.cs
+++ Assets/Scripts/Synchronizer.cs
@@ -6,7 +6,11 @@ using System.Collections;
 public class Synchronizer : Photon.MonoBehaviour {
 	protected Animator anim;
 	
-	
+	//受信データ
+	private Vector3 receivePosition = Vector3.zero;
+	private Quaternion receiveRotation = Quaternion.identity;
+	private Vector3 receiveVelocity = Vector3.zero;
+
 	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
 		// データを送る
 		if (stream.isWriting) {
@@ -24,9 +28,15 @@ public class Synchronizer : Photon.MonoBehaviour {
 			stream.SendNext(anim.GetFloat("GravityControl"));
 		} else {
 			//データの受信
+			/*
 			transform.position = (Vector3)stream.ReceiveNext();
 			transform.rotation = (Quaternion)stream.ReceiveNext();
 			rigidbody.velocity = (Vector3)stream.ReceiveNext();
+			*/
+			receivePosition = (Vector3)stream.ReceiveNext();
+			receiveRotation = (Quaternion)stream.ReceiveNext();
+			receiveVelocity = (Vector3)stream.ReceiveNext();
+
 			anim = GetComponent< Animator >();
 			anim.SetFloat("Speed",(float)stream.ReceiveNext());
 			anim.SetFloat("Direction",(float)stream.ReceiveNext());
@@ -36,5 +46,15 @@ public class Synchronizer : Photon.MonoBehaviour {
 			anim.SetFloat("GravityControl",(float)stream.ReceiveNext());
 		}
 	}
+
+	void Update() {
+		//自分以外のプレイヤーの補正
+		if(!photonView.isMine){
+			transform.position = Vector3.Lerp(transform.position, receivePosition, Time.deltaTime * 10);
+			transform.rotation = Quaternion.Lerp(transform.rotation, receiveRotation, Time.deltaTime * 10);
+			rigidbody.velocity = Vector3.Lerp(rigidbody2D.velocity, receiveVelocity, Time.deltaTime * 10);
+		}
+	}
+
 }
 
